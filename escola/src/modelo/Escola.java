8705cc28597d5998cdc63d735e3e640fe85705cd
Administrador <Administrador@10.10.4.11>
2019-08-08 16:47:13 -0300

    começado em 08/08/19

---
+++ escola/src/modelo/Escola.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Escola {
+    
+    private Integer codigo;
+    private String nome;
+    private String endereco;
+    private String sigla;
+    private Integer nalunos;
+    private Integer area;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Integer getNalunos() {
+        return nalunos;
+    }
+
+    public void setNalunos(Integer nalunos) {
+        this.nalunos = nalunos;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    @Override
+    public String toString() {
+        return  nome;
+    }
+   
+    
+}
